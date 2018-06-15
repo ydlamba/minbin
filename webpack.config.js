1329dae6491e0ae54d17f2f9cc11ed4260d5077c
Yash Dev Lamba <yashdev18@gmail.com>
2018-06-15 14:53:11 +0530

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CheckerPlugin } = require("awesome-typescript-loader");
+
+module.exports = (env, argv) => {
+  const config = {
+    entry: {
+      main: "./src/index.tsx"
+    },
+
+    output: {
+      filename: "[name].bundle.js",
+      chunkFilename: "[name].chunk.js",
+      path: path.resolve(__dirname, "dist")
+    },
+
+    devtool:
+      argv.mode === "development"
+        ? "cheap-module-eval-source-map"
+        : "source-map",
+
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: ["awesome-typescript-loader"]
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        }
+      ]
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "public/index.html"
+      }),
+      new CheckerPlugin()
+    ],
+
+    devServer: {
+      stats: {
+        colors: true
+      },
+      compress: true,
+      overlay: {
+        warnings: true,
+        errors: true
+      },
+      progress: true,
+      stats: "errors-only",
+      open: true,
+      contentBase: path.join(__dirname, "public"),
+      watchContentBase: true,
+      watchOptions: {
+        ignored: /node_modules/
+      }
+    },
+
+    node: {
+      fs: "empty"
+    }
+  };
+
+  return config;
+};
